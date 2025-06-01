646f886ab2764df1bb8ad05d1db39387c0425f2b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-01 15:19:49 +0200

    Implement units damaging one another

--- internal/update.go
+++ internal/update.go
@@ -34,22 +34,42 @@ func (self TeamState) update(other TeamState) TeamState {
 		new.Mana -= 1
 	}
 
+	new = new.updateUnits(other)
+
+	return new
+}
+
+func (self TeamState) updateUnits(other TeamState) TeamState {
+	new := self
+
+	const attackRange = 50
+	const attackDamage = 10
+
 	for i := range len(new.Units) {
 		target := other.Tower.Position
-		if rl.Vector2Distance(new.Units[i].Position, target) > 40 {
+		if rl.Vector2Distance(new.Units[i].Position, target) > attackRange-1 {
 			new.Units[i].Position = rl.Vector2MoveTowards(new.Units[i].Position, target, 4)
 		}
 	}
 
 	for _, u := range other.Units {
-		if rl.Vector2Distance(u.Position, self.Tower.Position) < 50 {
-			new.Tower.Hp -= 10
+		if rl.Vector2Distance(u.Position, self.Tower.Position) < attackRange {
+			new.Tower.Hp -= attackDamage
+		}
+		for i := range len(new.Units) {
+			if rl.Vector2Distance(u.Position, new.Units[i].Position) < attackRange {
+				new.Units[i].Hp -= attackDamage
+			}
 		}
 	}
 
-	if self.Tower.Hp <= 0 {
-		rl.CloseWindow()
+	newUnits := []Unit{}
+	for _, u := range new.Units {
+		if u.Hp > 0 {
+			newUnits = append(newUnits, u)
+		}
 	}
+	new.Units = newUnits
 
 	return new
 }

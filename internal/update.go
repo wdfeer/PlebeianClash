c7c7881cf5786ae1229dd11f6bc87099e753bb9a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-01 14:19:04 +0200

    tiny refactor

--- internal/update.go
+++ internal/update.go
@@ -3,25 +3,18 @@ package internal
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func (self GameState) Update() GameState {
-	return GameState{self.AState.update(self), self.BState.update(self)}
+	return GameState{self.AState.update(self.BState), self.BState.update(self.AState)}
 }
 
-func (self TeamState) update(game GameState) TeamState {
+func (self TeamState) update(other TeamState) TeamState {
 	new := self
 
 	if self.IsLocal && rl.IsMouseButtonPressed(rl.MouseLeftButton) {
 		new.Units = append(new.Units, Unit{Type: Knight, Hp: 300, Position: rl.GetMousePosition()})
 	}
 
-	var target rl.Vector2
-	if self.IsLocal == game.AState.IsLocal {
-		target = game.BState.Tower.Position
-	} else {
-		target = game.AState.Tower.Position
-	}
-
 	for i := range len(new.Units) {
-		new.Units[i].Position = rl.Vector2MoveTowards(new.Units[i].Position, target, 4)
+		new.Units[i].Position = rl.Vector2MoveTowards(new.Units[i].Position, other.Tower.Position, 4)
 	}
 
 	// TODO: update tower hp
